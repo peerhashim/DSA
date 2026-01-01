37e74bd4e7fbd36029e61419e50e4d843ad950ae
peerhashim369 <peerhashim369@gmail.com>
2026-01-01 22:08:28 +0530

    twosum brute force

---
+++ DSA/Problems/arrays/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Problems.arrays
+{
+    static class Arrays
+    {
+        // arrays - two sum
+        static public int[] TwoSumB(int[] nums, int target)
+        {
+            int last_index = nums.Length - 1;
+            int slow = 0;
+            int fast = 1;
+            for (; slow <= last_index; slow++)
+            {
+                for (; fast <= last_index; fast++)
+                {
+                    if (nums[slow] + nums[fast] == target)
+                    {
+                        return new[] { slow, fast };
+                    }
+                }
+                fast = slow + 2;
+            }
+
+            return new[] { -1, -1 };
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace DSA
+﻿using DSA.Problems.arrays;
+namespace DSA
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Arrays
+            // Two Sum Brute Force -- leetcode 1
+            var result = Arrays.TwoSumB(new int[] { 12, 2 }, 9);
+            PrintAny(result);
         }
+        static void PrintAny(object? result)
+        {
+            if (result == null)
+            {
+                Console.WriteLine("null");
+                return;
+            }
+
+            // Handle collections (array, list, etc.)
+            if (result is System.Collections.IEnumerable enumerable && result is not string)
+            {
+                Console.Write("[ ");
+                bool first = true;
+
+                foreach (var item in enumerable)
+                {
+                    if (!first) Console.Write(", ");
+                    Console.Write(item);
+                    first = false;
+                }
+
+                Console.WriteLine(" ]");
+                return;
+            }
+
+            // Handle objects, tuples, anonymous types, primitives
+            Console.WriteLine(result);
+        }
+
     }
+
+
 }
